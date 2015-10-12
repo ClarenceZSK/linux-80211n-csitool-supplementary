e686a455004f5da401bb9ed1c5ee11f91fe0028d
libing64 <libing64@gmail.com>
2015-10-12 17:04:30 +0800

    add .h file and .sh file

--- injection/setup_monitor_csi.sh
+++ injection/setup_monitor_csi.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/sudo /bin/bash
 modprobe -r iwlwifi mac80211 cfg80211
-modprobe iwlwifi connector_log=0x1
+#modprobe iwlwifi connector_log=0x1 
+modprobe iwlwifi connector_log=0x5 #cis + package payload
+
 # Setup monitor mode, loop until it works
-iwconfig wlan0 mode monitor 2>/dev/null 1>/dev/null
+iwconfig wlan1 mode monitor 2>/dev/null 1>/dev/null
 while [ $? -ne 0 ]
 do
-	iwconfig wlan0 mode monitor 2>/dev/null 1>/dev/null
+	iwconfig wlan1 mode monitor 2>/dev/null 1>/dev/null
 done
-iw wlan0 set channel $1 $2
-ifconfig wlan0 up
+iw wlan1 set channel $1 $2
+ifconfig wlan1 up
